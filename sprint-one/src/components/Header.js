d830235ad0b9653cc9a6c9e4a02c4ae64b6da6e2
Nusorn <nkrachangtoy@gmail.com>
2020-05-11 19:30:47 -0700

    Added nav html

--- sprint-one/src/components/Header.js
+++ sprint-one/src/components/Header.js
@@ -3,7 +3,31 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1>Header</h1>
+      <nav className="nav">
+        <a href="#" className="nav__logo">
+          <img src="assets/Logo/Logo-brainflix.svg" alt="BrainFlix Logo" />
+        </a>
+        <ul className="nav__list">
+          <li className="nav__item">
+            <form>
+              <input
+                type="text"
+                className="nav__item--search-box"
+                placeholder="Search"
+                name="search"
+              />
+            </form>
+          </li>
+          <li className="nav__item">
+            <button type="submit" className="nav__item--btn"></button>
+          </li>
+          <li className="nav__item">
+            <div className="nav__item--circle-container">
+              <img src="" alt="Profile Icon" />
+            </div>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
