2139f659d76b3a6fe925fb99199a0d264dc3cb02
Nusorn <nkrachangtoy@gmail.com>
2020-07-09 09:37:20 -0700

    add gitignore to server

--- sprint-three/server/controller/uploadVideo.js
+++ sprint-three/server/controller/uploadVideo.js
@@ -1,22 +1,23 @@
-// const fs = require("fs");
-// const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
-// // Load data
-// const videos = JSON.parse(fs.readFileSync("./model/videos.json"));
+// Load data
+const videos = JSON.parse(fs.readFileSync("./model/videos.json"));
 
-// // Function to save videos, read/write videos.json
-// const uploadVideo = (videoData) => {
-//   const video = {
-//     id: uuidv4(),
-//     title: videoData.title,
-//     description: videoData.description,
-//     image: videoData.image,
-//   };
+// Function to save videos, read/write videos.json
+const uploadVideo = (videoData) => {
+  const video = {
+    id: uuidv4(),
+    title: videoData.title,
+    description: videoData.description,
+    image: videoData.image,
+    timestamp: newDate(),
+  };
 
-//   videos.push(video);
+  videos.push(video);
 
-//   fs.writeFileSync("./model/videos.json", JSON.stringify(videos));
-//   return videos;
-// };
+  fs.writeFileSync("./model/videos.json", JSON.stringify(videos));
+  return videos;
+};
 
-// module.exports = uploadVideo;
+module.exports = uploadVideo;
